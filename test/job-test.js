d2e9b8f61cef3b347476c421b477e59b37daf299
Matthias Kling <m.kling@sprax.eu>
2013-01-09 16:47:12 +0100

    + fail: job.js should export a function named create

--- test/job-test.js
+++ test/job-test.js
@@ -3,3 +3,11 @@ var assert = buster.assert;
 var refute = buster.refute;
 
 var job = require("../lib/job.js");
+
+buster.testCase("job", {
+
+    "exports function create": function() {
+        assert.isFunction(job.create);
+    }
+
+});
