12d4bc50efedcfcd7409b12d2be34e13b5e7a17d
Matthias Kling <m.kling@sprax.eu>
2013-01-09 19:21:40 +0100

    * refactor tests

--- test/job-test.js
+++ test/job-test.js
@@ -6,51 +6,58 @@ var job = require("../lib/job.js");
 
 buster.testCase("job", {
 
-    ".create() returns a function that": {
+    ".create()": {
+    
+        "returns a function that": {
 
-        "won't throw if given no argument": function() {
-            var j = job.create();
-            refute.exception(j, "should not have thrown");
+            "won't throw if given no argument": function() {
+                var j = job.create();
+                refute.exception(j, "should not have thrown");
+            },
+
+            "will call its argument": function() {
+                var f = this.spy();
+                var j = job.create();
+                j(f);
+                assert.calledOnce(f);
+            },
+
         },
 
-        "will call its argument": function() {
-            var f = this.spy();
-            var j = job.create();
-            j(f);
-            assert.calledOnce(f);
-        },
-
-        "has a property 'then' which is a function": function() {
+        ".then is a function": function() {
             var j = job.create();
             assert.isFunction(j.then);
         },
-
     },
 
-    ".create(f) returns a function that": {
+    ".create(f)": {
+    
+        "returns a function that": {
 
-         "will, when called with": {
-         
-            "no arg, call f with a function arg": function() {
-                var f = this.spy();
-                var j = job.create(f);
-                j();
-                assert.calledOnce(f);
-                assert.isFunction(f.args[0][0]); // first arg of first call to f
-            },
+             "will, when called with": {
+             
+                "no arg, call f with a function arg": function() {
+                    var f = this.spy();
+                    var j = job.create(f);
+                    j();
+                    assert.calledOnce(f);
+                    assert.isFunction(f.args[0][0]); // first arg of first call to f
+                },
 
-             "a function arg, call f with that arg": function() {
-                var f = this.spy();
-                var j = job.create(f);
-                var g = function() {};
-                j(g);
-                assert.calledOnce(f);
-                assert.calledWithExactly(f, g);
+                 "a function arg, call f with that arg": function() {
+                    var f = this.spy();
+                    var j = job.create(f);
+                    var g = function() {};
+                    j(g);
+                    assert.calledOnce(f);
+                    assert.calledWithExactly(f, g);
+                },
+
             },
 
         },
-
-        "has a property 'then' which is a function": function() {
+        
+        ".then is a function": function() {
             var f = function() {};
             var j = job.create(f);
             assert.isFunction(j.then);
