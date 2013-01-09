bab9422b1aaf3de00d29d89554081c0e2b0c28ec
Matthias Kling <m.kling@sprax.eu>
2013-01-09 20:38:32 +0100

    * fail: giving job.create(..) anything but a function arg makes no sense either

--- test/job-test.js
+++ test/job-test.js
@@ -6,50 +6,18 @@ var job = require("../lib/job.js");
 
 buster.testCase("job", {
 
-    ".create()": {
-    
-        "returns a function that": {
+    ".create(..) throws when given": {
 
-            "won't throw if given no argument": function() {
-                var j = job.create();
-                refute.exception(j, "should not have thrown");
-            },
-
-            "will call its argument": function() {
-                var f = this.spy();
-                var j = job.create();
-                j(f);
-                assert.calledOnce(f);
-            },
-
+        "no arg at all": function() {
+            assert.exception(function () { job.create(); }, "no arg");
         },
 
-        ".then(..) throws when not given any arg": function() {
-            var j = job.create();
-            assert.exception(function () { j.then(); }, "no arg");
-        },
-
-        ".then(..) throws when given a non-function arg": function() {
-            var j = job.create();
-            assert.exception(function () { j.then(null); }, "null");
-            assert.exception(function () { j.then(undefined); }, "undefined");
-            assert.exception(function () { j.then(0); }, "0");
-            assert.exception(function () { j.then(''); }, "empty string");
-            assert.exception(function () { j.then({}); }, "empty object");
-        },
-
-        ".then(h) returns the job itself": function() {
-            var j = job.create();
-            var h = function () {};
-            var j2 = j.then(h); // act
-            assert.same(j2, j);
-        },
-
-        "//.then(h)() calls h": function() {
-            var h = this.spy();
-            var j = job.create().then(h);
-            j(); // act
-            assert.calledOnce(h);
+        "a non-function arg": function() {
+            assert.exception(function () { job.create(null); }, "null");
+            assert.exception(function () { job.create(undefined); }, "undefined");
+            assert.exception(function () { job.create(0); }, "0");
+            assert.exception(function () { job.create(''); }, "empty string");
+            assert.exception(function () { job.create({}); }, "empty object");
         },
         
     },
