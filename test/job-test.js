edf20a4eed381db74c297d2397e49cb72f4f0b8d
Matthias Kling <m.kling@sprax.eu>
2013-01-10 18:01:40 +0100

    * (pass) refactored tests, workaround stuff put into this.spyX, in setUp

--- test/job-test.js
+++ test/job-test.js
@@ -35,28 +35,27 @@ buster.testCase("job", {
     },
 
     ".create(f)": {
-         
-        "() calls f with a function arg": function() {
-            var f = this.spy();
-            var j = job.create(f);
-            j();
-            assert.calledOnce(f);
-            assert.isFunction(f.args[0][0]); // first arg of first call to f
+
+        setUp: function () {
+            var self = this;
+            this.callChain = "";
+            this.spyX = function (name, maxCallcount, f) {
+                var callCount = 0;
+                return self.spy( function () {
+                    self.callChain += "->" + name;
+                    if (++callCount > maxCallcount)
+                        throw new Error(name + " called " + callCount + " times! (" + self.callChain+ ")");
+                    return f.apply(this, arguments);
+                });
+            }
+            this.f_callsIts1stArg = function (done) { done(); };
+            this.f_doesNothing = function () {};
         },
          
         "(..) throws TypeError when given a non-function arg": function() {
             var f = this.spy();
             var j = job.create(f);
             this.assertTypeErrorOnNonFunctionArg(j);
-        },
-
-         "(h), h a function, calls f with h": function() {
-            var f = this.spy();
-            var j = job.create(f);
-            var h = function() {};
-            j(h);
-            assert.calledOnce(f);
-            assert.calledWithExactly(f, h);
         },
 
         ".then() throws TypeError": function() {
@@ -70,61 +69,66 @@ buster.testCase("job", {
             var j = job.create(f);
             this.assertTypeErrorOnNonFunctionArg(j.then);
         },
-
-        ".then(g), g a function, returns the job itself": function() {
-            var f = function () {};
+         
+        "() calls f with a function arg": function() {
+            var f = this.spy();
             var j = job.create(f);
-            var g = function () {};
-            var j2 = j.then(g); // act
-            assert.same(j2, j);
-        },
-
-        ".then(g)(h), g & h functions, calls f, then g and finally h": function() {
-            var f = this.spy( function (done) { done(); } );
-            var g = this.spy();
-            var h = this.spy();
-            var j = job.create(f).then(g);
-            j(h); // act
+            j();
             assert.calledOnce(f);
-            assert.calledOnce(g);
-            assert.calledOnce(h);
-            assert.callOrder(f, g, h);
+            assert.isFunction(f.args[0][0]); // first arg of first call to f
         },
 
-        ".then(g).then(h)(i), g, h & i functions, calls f, then g then h and finally i": function() {
-            var callChain = "";
-            var self = this;
-            function makeTestFn(name, maxCallcount, f) {
-                var callCount = 0;
-                return self.spy( function () {
-                    callChain += "->" + name;
-                    if (++callCount > maxCallcount)
-                        throw new Error(name + " called " + callCount + " times! (" + callChain+ ")");
-                    return f.apply(this, arguments);
-                });
-            }
-            var a = function (done) { done(); }
-            var b = function () {};
+         "(h), h a function, calls f with h": function() {
+            var f = this.spy();
+            var j = job.create(f);
+            var h = function() {};
+            j(h);
+            assert.calledOnce(f);
+            assert.calledWithExactly(f, h);
+        },
+
+        ".then(g)": {
+
+            ", g a function, returns the job itself": function() {
+                var f = function () {};
+                var g = function () {};
+                var j = job.create(f);
+                var j2 = j.then(g); // act
+                assert.same(j2, j);
+            },
+
+            "(h), g & h functions, calls f, then g and finally h": function() {
+                var f = this.spy( function (done) { done(); } );
+                var g = this.spy();
+                var h = this.spy();
+                var j = job.create(f).then(g);
+                j(h); // act
+                assert.calledOnce(f);
+                assert.calledOnce(g);
+                assert.calledOnce(h);
+                assert.callOrder(f, g, h);
+            },
             
-            var f = makeTestFn("f", 1, a);
-            var g = makeTestFn("g", 1, a);
-            var h = makeTestFn("h", 1, b);
-            var i = makeTestFn("i", 1, b);
-            var j = makeTestFn("j", 1, job.create(f).then(g).then(h) );
+            ".then(h)(i), g, h & i functions, calls f, then g then h and finally i": function() {
+                var f = this.spyX("f", 1, this.f_callsIts1stArg);
+                var g = this.spyX("g", 1, this.f_callsIts1stArg);
+                var h = this.spyX("h", 1, this.f_doesNothing);
+                var i = this.spyX("i", 1, this.f_doesNothing);
+                var j = this.spyX("j", 1, job.create(f).then(g).then(h) );
 
-            try {
-                j(i);
-            } catch(e) {
-                throw new Error("callChain: " + callChain + "; " + util.inspect(e, true));
-            }
+                try {
+                    j(i);
+                } catch(e) {
+                    throw new Error("callChain: " + this.callChain + "; " + util.inspect(e, true) + "\n" + e.stack);
+                }
 
-            assert.equals(f.callCount, 1, callChain + "; f.callCount");
-            assert.equals(g.callCount, 1, callChain + "; g.callCount");
-            assert.equals(h.callCount, 1, callChain + "; h.callCount");
-            assert.equals(i.callCount, 1, callChain + "; i.callCount");
-            assert.callOrder(f, g, h, i);
+                assert.equals(f.callCount, 1, this.callChain + "; f.callCount");
+                assert.equals(g.callCount, 1, this.callChain + "; g.callCount");
+                assert.equals(h.callCount, 1, this.callChain + "; h.callCount");
+                assert.equals(i.callCount, 1, this.callChain + "; i.callCount");
+                assert.callOrder(f, g, h, i);
+            },
+
         },
-
     },
-    
 });
